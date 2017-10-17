2bf9ebddca01c9b61cc31acf472597e764c726bd
xycyx <yxcheng@uw.edu>
2017-10-17 01:38:06 -0700

    Add table

--- test.py
+++ test.py
@@ -28,6 +28,8 @@ for file in os.listdir("./example"):
 img_name = ['0','1','2','3','4','5','6','7']
 img_nums = 6
 prs = add_new_slide_pics(prs, img_paths, img_nums, img_name)
+
+add_a_table(prs, 4, 3, 'test the table')
 prs.save('test1.pptx')
 #%%
 def add_pic_with_title(slide, img_path, left, top, width, img_name):
@@ -63,7 +65,7 @@ def add_new_slide_pics(prs, img_paths, img_nums, img_name):
         layout = [1, 3]
         prs = set_layout(prs, layout, img_paths, img_nums, img_name)
    
-    #%% 4
+    #%% 4 still have a issue
     elif img_nums == 4:
         layout = [2, 2]
         prs = set_layout(prs, layout, img_paths, img_nums, img_name)
@@ -105,8 +107,90 @@ def set_layout(prs, layout,img_paths, img_nums, img_name):
         add_pic_with_title(slide, img_path, left, top, pic_width, img_name[ind])
         ind = ind + 1
     return prs
+ #%%   
+from PIL import Image
+im = Image.open('./example/1.tiff')
+im.size
+
+
+width, height = get_image_size('./example/1.jpg')
+import struct
+import imghdr
+
+def get_image_size(fname):
+    '''Determine the image type of fhandle and return its size.
+    from draco'''
+    with open(fname, 'rb') as fhandle:
+        head = fhandle.read(24)
+        if len(head) != 24:
+            return
+        if imghdr.what(fname) == 'png':
+            check = struct.unpack('>i', head[4:8])[0]
+            if check != 0x0d0a1a0a:
+                return
+            width, height = struct.unpack('>ii', head[16:24])
+        elif imghdr.what(fname) == 'gif':
+            width, height = struct.unpack('<HH', head[6:10])
+        elif imghdr.what(fname) == 'bmp':
+            _, width, height, depth = re.search(
+                b"((\d+)\sx\s"
+                b"(\d+)\sx\s"
+                b"(\d+))", str).groups()
+            width = int(width)
+            height = int(height)
+        elif imghdr.what(fname) == 'jpeg':
+            try:
+                fhandle.seek(0) # Read 0xff next
+                size = 2
+                ftype = 0
+                while not 0xc0 <= ftype <= 0xcf:
+                    fhandle.seek(size, 1)
+                    byte = fhandle.read(1)
+                    while ord(byte) == 0xff:
+                        byte = fhandle.read(1)
+                    ftype = ord(byte)
+                    size = struct.unpack('>H', fhandle.read(2))[0] - 2
+                # We are at a SOFn block
+                fhandle.seek(1, 1)  # Skip `precision' byte.
+                height, width = struct.unpack('>HH', fhandle.read(4))
+            except Exception: #IGNORE:W0703
+                return
+        else:
+            return
+        return width, height    
+#%%    
+# add table
+
+
+def add_a_table(prs, rows, cols, slide_title):
+    from pptx.util import Inches
+    title_only_slide_layout = prs.slide_layouts[5]
+    slide = prs.slides.add_slide(title_only_slide_layout)
+    shapes = slide.shapes
     
+    shapes.title.text = slide_title
     
+#    rows = cols = 2
+    left = top = Inches(2.0)
+    width = Inches(6.0)
+    height = Inches(0.8)
+    
+    table = shapes.add_table(rows, cols, left, top, width, height).table
+    
+#    # set column widths
+#    table.columns[0].width = Inches(2.0)
+#    table.columns[1].width = Inches(4.0)
+#    
+#    # write column headings
+#    table.cell(0, 0).text = 'Foo'
+#    table.cell(0, 1).text = 'Bar'
+#    
+#    # write body cells
+#    table.cell(1, 0).text = 'Baz'
+#    table.cell(1, 1).text = 'Qux'
+    return prs
+
+
 ######################################################
 #%% test part
 from pptx import Presentation
@@ -215,5 +299,9 @@ tf = txBox.text_frame
 
 tf.text = img_path[10::]
     
+#%% main
+import argparse
+if __name__ == "__main__":
+    args = parse_args()
     
     
