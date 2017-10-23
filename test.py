1972fb9399c276fbd032484c4659637cabf0149c
xycyx <yxcheng@uw.edu>
2017-10-22 17:20:33 -0700

    Add a slide

--- test.py
+++ test.py
@@ -6,31 +6,6 @@ Created on Thu Oct 12 16:14:01 2017
 @author: yxcheng
 """
 
-
-#%%
-from pptx import Presentation
-prs = Presentation()
-#blank_slide_layout = prs.slide_layouts[6]
-#slide = prs.slides.add_slide(blank_slide_layout)
-#img_path = './example/Picture4.png'
-#add_pic_with_title(slide, img_path, 5, 4.5, 2.5, 'Picture4.png')
-#prs.save('test2.pptx')
-
-import os
-img_paths = []
-for file in os.listdir("./example"):
-    if file.endswith(".png"):
-        print(os.path.join("./example", file))
-        img_paths.append(os.path.join("./example", file))
-
-
-
-img_name = ['0','1','2','3','4','5','6','7']
-img_nums = 6
-prs = add_new_slide_pics(prs, img_paths, img_nums, img_name)
-
-add_a_table(prs, 4, 3, 'test the table')
-prs.save('test1.pptx')
 #%%
 def add_pic_with_title(slide, img_path, left, top, width, img_name):
     """add a picture to slide from img_path and add the title with img_name
@@ -231,10 +206,10 @@ def search_images_in_folder(directory):
                 file_lists.append(os.path.join(root, file))
     return file_lists       
  
-file_lists = search_images_in_folder('./example2')
+#file_lists = search_images_in_folder('./example2')
 
 #%%            
-import re
+#import re
 file_name = file_lists[0]
 file_name = clean_file_name(file_name)
 
@@ -569,8 +544,7 @@ def filter_images_by_features(df, system_type=0, OD_OS=0, FOV=0,
         
     return df_new
     
-
-    
+   
 def get_images_information(df):
     unique_scan_time = df['scan_time'].unique()
     unique_FOV = df['FOV'].unique()
@@ -580,26 +554,101 @@ def get_images_information(df):
     return images_information
 
 
-
-def build_the_slide(df, style_type, scan_time):
+def build_the_slide(prs, df, style_type, scan_time):
     img_paths = []
-    img_names
+    img_names = []
     if style_type=='Retina':
         #insert the retina slide here
-        img_nums = 6
-        img_names.append('VRI')
-        df_img1 = filter_images_by_features(df, scan_time=scan_time, 
-                                  image_modality='Angiography', 
-                                  image_layer=='VRI')
-        img_paths.append(df_img1['img_path'])
+        img_nums = 8
+
+        image_col = ['VRI', 'Superficial', 'Deep', 'Retina']
+        image_row = ['Angiography','Structure']
+        for row in range(0, 2):
+            for col in range(0, 4):
+                img_names.append(image_col[col])
+                df_img = filter_images_by_features(df, scan_time=scan_time, 
+                                          image_modality=image_row[row], 
+                                          image_layer=image_col[col])
+                img_paths.append(df_img['img_path'])
         
-        
-        prs = add_new_slide_pics(prs, img_paths, img_nums, img_names)
+        if len(img_paths)<=img_nums:
+            prs = add_new_slide_pics(prs, img_paths, img_nums, img_names, 'Title')
+        else:
+            print ('img_paths have ', len(img_paths), 'larger than 8')
         
         
     elif  style_type=='CC':
         #insert another slide here
-    
+        img_nums = 8
+
+        image_col = ['Avascular', 'Custom', 'Choroid', 'RetinaDepthEncoded']
+        image_row = ['Angiography','Structure']
+        for row in range(0, 2):
+            for col in range(0, 4):
+                # the last image is B-scanflow
+                if row==1 & col ==3:
+                    image_col[col] = 'B-ScanFlow'
+                    image_row[row] = 'Angiography'
+                
+                img_names.append(image_col[col])
+                df_img = filter_images_by_features(df, scan_time=scan_time, 
+                                          image_modality=image_row[row], 
+                                          image_layer=image_col[col])
+                img_paths.append(df_img['img_path'])
+                
+        if len(img_paths)<=img_nums:
+            prs = add_new_slide_pics(prs, img_paths, img_nums, img_names,'Title')
+        else:
+            print ('img_paths have ', len(img_paths), 'larger than 8')
+            
+    return prs
+#%%
+######################################################
+#main    
+
+
+#%%
+from pptx import Presentation
+prs = Presentation()
+#blank_slide_layout = prs.slide_layouts[6]
+#slide = prs.slides.add_slide(blank_slide_layout)
+#img_path = './example/Picture4.png'
+#add_pic_with_title(slide, img_path, 5, 4.5, 2.5, 'Picture4.png')
+#prs.save('test2.pptx')
+
+import os
+img_paths = []
+for file in os.listdir("./example"):
+    if file.endswith(".png"):
+        print(os.path.join("./example", file))
+        img_paths.append(os.path.join("./example", file))
+
+
+
+img_name = ['0','1','2','3','4','5','6','7']
+img_nums = 6
+prs = add_new_slide_pics(prs, img_paths, img_nums, img_name)
+
+add_a_table(prs, 4, 3, 'test the table')
+prs.save('test1.pptx')
+
+
+
+#########
+from pptx import Presentation
+import re
+
+prs = Presentation()
+
+file_lists = search_images_in_folder('./example2')
+df = analysis_filelists(file_lists)
+prs = build_the_slide(prs, df, 'Retina', scan_time)
+
+prs.save('test2.pptx')
+
+
+
+
                 
 ######################################################
 #%% test part
